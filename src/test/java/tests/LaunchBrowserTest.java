f351eb5e93cd3b55d2d4612ff1c7f9afe7620cbf
sreeramgkrishnan <sreeramgkrishnan@gmail.com>
2026-03-30 23:41:00 -0400

    Sample test

---
+++ src/test/java/tests/LaunchBrowserTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.annotations.Test;
+
+public class LaunchBrowserTest extends BaseTest {
+    @Test
+    public void launchBrowser() {
+
+    }
+}
